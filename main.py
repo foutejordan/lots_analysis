ebcdcbf4e0bba173a8b9bf68b2b4154f8879b9aa
foutejordan <2ftech20@gmail.com>
2024-03-12 17:26:12 +0100

    nothing

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ import analyse_bivarie1
 df_lots = pd.read_csv('data/Lots.csv', dtype=str)
 df_lots['cpv_name'] = df_lots['cpv'].astype(str).str[:2]
 
+#
 if __name__ == '__main__':
     analyse_bivarie1.execute_file(df_lots)
