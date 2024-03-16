9be154f9e3fe3ed1a410c67f69604116b292c05c
foutejordan <2ftech20@gmail.com>
2024-03-16 14:34:32 +0100

    univariate analysis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import analyse_bivarie1
+import analyse1
 
 df_lots = pd.read_csv('data/Lots.csv', dtype=str)
 df_lots['cpv_name'] = df_lots['cpv'].astype(str).str[:2]
@@ -7,3 +8,4 @@ df_lots['cpv_name'] = df_lots['cpv'].astype(str).str[:2]
 #
 if __name__ == '__main__':
     analyse_bivarie1.execute_file(df_lots)
+    analyse1.execute_file(df_lots)
